34f59e4abc870bd2cb25c485780a68db3fdc0d9c
= <mfittazim@gmail.com>
2024-10-03 12:06:42 +0600

    server connection completed

--- server/index.js
+++ server/index.js
@@ -1,51 +1,70 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const jwt = require("jsonwebtoken");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
 const port = process.env.PORT || 5000;
+require("dotenv").config();
+var jwt = require("jsonwebtoken");
 
-const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions))
-
-app.use(express.json())
-// app.use(cookieParser())
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://auth-master.netlify.app"],
+    credentials: true,
+  })
+);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o4eqbyc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   },
-})
+});
+
+const verifyToken = (req, res, next) => {
+  // console.log('inside = ', req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "Forbidden-Access" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  // console.log(token)
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(401).send({ message: "Forbidden-Access" });
+    }
+    req.decoded = decoded;
+    //console.log("decoded value = ", decoded);
+
+    next();
+  });
+};
 
 async function run() {
   try {
-    const db = client.db('airbnb')
-    const roomsCollection = db.collection('rooms')
-   
-
+    // Connect the client to the server	(optional starting in v4.7)
+    //await client.connect();
+  
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
+    await client.db("admin").command({ ping: 1 });
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
+    //await client.close();
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello from StayVista Server..')
-})
+app.get("/", (req, res) => {
+  res.send("server!");
+});
 
 app.listen(port, () => {
-  console.log(`Airbnb is running on port ${port}`)
-})
+  console.log(`server is listening on port ${port}`);
+});
